80546671181290784364e031ea3fc01fd47b0a16
lab-zuston <zuston.shacha@gmail.com>
2016-09-22 19:09:28 +0800

    增加题目

--- 121.c
+++ 121.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+
 //暴力解法
 // int maxProfit(int* prices, int pricesSize) {
 //
@@ -19,6 +21,14 @@
 //     return max;
 //
 // }
+
+
+// Say you have an array for which the ith element is the price of a given stock on day i.
+// If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+// Input: [7, 1, 5, 3, 6, 4]
+// Output: 5
+//
+// max. difference = 6-1 = 5 (not 7-1 = 6, as selling price needs to be larger than buying price)
 
 int maxProfit(int *prices,int pricesSize){
     if(pricesSize<2)    return 0;
