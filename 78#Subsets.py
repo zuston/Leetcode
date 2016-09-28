d1fad589acfd4c9c58ac766f1ae2828b42a9b483
Zjf <zuston.shacha@gmail.com>
2016-09-28 23:19:28 +0800

    Update 78#Subsets.py

--- 78#Subsets.py
+++ 78#Subsets.py
@@ -12,7 +12,6 @@ class Solution(object):
     def loop(self,nums,step,temp):
         if step==len(nums):
             self.res.append(temp)
-
             return
         self.loop(nums,step+1,temp)
         temp = temp+[nums[step]]
