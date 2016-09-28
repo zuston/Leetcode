fd8a0c65b1b6c3113a85ff9b849f11176f64d33c
zuston.shacha <zuston.shacha@gmail.com>
2016-09-28 23:03:22 +0800

    数组

--- 81#Search-in-Rotated-Sorted-Array-II.c
+++ 81#Search-in-Rotated-Sorted-Array-II.c
@@ -1,17 +1,30 @@
 #include <stdlib.h>
 
+bool binarySearch(int *nums,int start,int end,int target){
+    while(end>=start){
+        int mid = (end+start)/2;
+        if(nums[mid]==target){
+            return true;
+        }
+        if(nums[mid]<target){
+            start = mid+1;
+        }
+        if(nums[mid]>target){
+
+            end = mid-1;
+        }
+    }
+
+    return false;
+}
+
 bool search(int* nums, int numsSize, int target) {
     int i=0;
     while(nums[i]<=nums[i+1]&&i<numsSize-2){
         i++;
     }
-    k = i;
+    printf("splir point=%d\n",i);
     return binarySearch(nums,0,i,target) || binarySearch(nums,i+1,numsSize-1,target);
-}
-
-bool binarySearch(int *nums,int start,int end,int target){
-    int mid = (start+end)/2;
-    while(mid)
 }
 
 int main(int argc, char const *argv[]) {
