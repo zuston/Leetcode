f6bfecbc8dafc12f7097c9299dbe122b12b718af
lab-zuston <zuston.shacha@gmail.com>
2016-09-28 20:58:38 +0800

    数组

---
+++ 26#Remove-Duplicates-from-Sorted-Array.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+
+int removeDuplicates(int* nums, int numsSize) {
+    if(numsSize==0) return 0;
+    int index=0;
+    for(int i=1;i<numsSize;i++){
+        if(nums[index]!=nums[i]){
+            nums[++index] = nums[i];
+        }
+    }
+    return index;
+}
+
+int main(int argc, char const *argv[]) {
+    /* code */
+    return 0;
+}
